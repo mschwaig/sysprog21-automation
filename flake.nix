4285d8b77824e03249da4f7544c87e231061d758
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2022-05-24 22:26:37 +0200

    move eval script to expose it to other components
    Doing this se the eval script can be called from my pylti project.

--- flake.nix
+++ flake.nix
@@ -121,24 +121,18 @@
           }
         ];
       };
+      eval-script = (pkgs.writeScriptBin "eval-submission" (''
+        #!${pkgs.runtimeShell}
+        expr='import ${./eval-script-function.nix} { mkAssignment = (builtins.getFlake "${self}").lib.mkAssignment; meta={name="${meta.name}";rev="${meta.rev}";}; binary-name="${binary-name}"; src-names=[${pkgs.lib.strings.concatMapStrings (x: "\"${x}\" ") src-names}]; reference={ src=${reference.src}; skip = ${builtins.toString reference.skip}; files = [${pkgs.lib.strings.concatMapStrings (x: "\"${x}\" ") reference.files}]; }; ref-data=${ref-data}; config=${config}; student-submission = '"$1"';} '
+        exec nix build --no-link -L --impure --expr "$expr"
+      ''));
       in rec {
-
-      # command line access to the eval script is only needed for testing
-      # eval = {
-      #   type = "app";
-      #   program = eval-script;
-      # };
-
+        inherit eval-script;
       server = {
         type = "app";
         program =
           let
             server-binary = self.packages."${system}".server + "/bin/live-feedback-server";
-            eval-script = (pkgs.writeScriptBin "eval-submission" (''
-              #!${pkgs.runtimeShell}
-              expr='import ${./eval-script-function.nix} { mkAssignment = (builtins.getFlake "${self}").lib.mkAssignment; meta={name="${meta.name}";rev="${meta.rev}";}; binary-name="${binary-name}"; src-names=[${pkgs.lib.strings.concatMapStrings (x: "\"${x}\" ") src-names}]; reference={ src=${reference.src}; skip = ${builtins.toString reference.skip}; files = [${pkgs.lib.strings.concatMapStrings (x: "\"${x}\" ") reference.files}]; }; ref-data=${ref-data}; config=${config}; student-submission = '"$1"';} '
-              exec nix build --no-link -L --impure --expr "$expr"
-            '')) + "/bin/eval-submission";
           in (pkgs.writeScriptBin "run_server" ''
           ${server-binary} -e ${eval-script}
         '') + "/bin/run_server";
